7685d33716095540714e7f4001ec5dbc27814469
MillerMoreno87 <milprogjaxx@gmail.com>
2023-11-25 17:39:54 -0500

    EcoRecicla v2

--- app/src/main/java/com/example/ecorecicla/ResgistroUsuarioActivity2.java
+++ app/src/main/java/com/example/ecorecicla/ResgistroUsuarioActivity2.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ResgistroUsuarioActivity2 extends AppCompatActivity {
 
+    EditText nombreCom, correo, password;
     Button registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resgistro_usuario2);
+
+        nombreCom= findViewById(R.id.editTextText2);
+        correo= findViewById(R.id.editTextTextR);
+        password= findViewById(R.id.editTextTextPasswordR);
+
 
         registrar=findViewById(R.id.buttonRegist);
 
@@ -24,6 +31,8 @@ public class ResgistroUsuarioActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(inicio);
+                System.out.println(nombreCom.getEditableText().toString()+" "+correo.getEditableText().toString()
+                        +" "+password.getText().toString());
             }
         });
     }
