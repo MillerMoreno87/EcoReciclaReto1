90f236a37e8b809b279117ae21c46dcefc9d9b6b
MillerMoreno87 <milprogjaxx@gmail.com>
2023-11-26 22:07:30 -0500

    EcoRecicla v2.0

--- app/src/main/java/com/example/ecorecicla/RegistroitemActivity2.java
+++ app/src/main/java/com/example/ecorecicla/RegistroitemActivity2.java
@@ -3,12 +3,21 @@ package com.example.ecorecicla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistroitemActivity2 extends AppCompatActivity {
 
+    EditText mess,elementoo,precioo;
+    Button  envReg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registroitem2);
+
+        mess=findViewById(R.id.eTxRmez);
+        elementoo=findViewById(R.id.eTxpcvt);
+        precioo=findViewById(R.id.eTxprco);
+        envReg=findViewById(R.id.btnenvReg);
     }
 }
